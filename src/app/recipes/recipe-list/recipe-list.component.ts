79e3d4550ad5355cccc4c575750c4df93d33e7b7
Kumar <726124@cognizant.com>
2019-02-04 16:40:34 +0530

    course chapter 1 (add, delete, clear button spacing not corect)

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simple a test', 'https://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg')
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
